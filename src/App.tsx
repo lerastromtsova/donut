1b91d5240a24bf84eae3a816a89354e79cf45be0
Valeria Stromtsova <v.stromtsova@yandex.ru>
2020-11-14 16:08:47 +0300

    Revert

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
-
-import StreamerPage from "./pages/StreamerPage";
-import DonateToStreamerPage from "./pages/DonateToStreamerPage";
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import "./App.css"
 
+import Sidebar from "./components/Sidebar";
+import ProfilePage from "./pages/ProfilePage";
+import DonateLinkPage from "./pages/DonateLinkPage";
+import GoalsPage from "./pages/GoalsPage";
+import NotificationsPage from "./pages/NotificationsPage";
+import StatsPage from "./pages/StatsPage";
+
 function App() {
   return (
-      <Switch>
-          <Route exact path='/donate_to/:nickname' component={DonateToStreamerPage} />
-          <Route component={StreamerPage} />
-      </Switch>
+      <div className="wrapper d-flex align-items-stretch">
+          <Sidebar/>
+          <div className="main p-4 p-md-5 pt-5">
+              <Switch>
+                  <Route path='/profile' component={ProfilePage} />
+                  <Route exact path='/donate' component={DonateLinkPage} />
+                  <Route exact path='/goals' component={GoalsPage} />
+                  <Route exact path='/notifications' component={NotificationsPage} />
+                  <Route exact path='/stats' component={StatsPage} />
+                  <Redirect to='/' />
+              </Switch>
+          </div>
+      </div>
   );
 }
 
