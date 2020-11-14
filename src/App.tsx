f790e8c5d1f2ccd872c43f077dc08cf885e98949
Valeria Stromtsova <v.stromtsova@yandex.ru>
2020-11-14 11:31:56 +0300

    Fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi, Petya, Amazat and Salavat!
+          Hi, Petya, Azamat and Salavat!
         </p>
         <a
           className="App-link"
