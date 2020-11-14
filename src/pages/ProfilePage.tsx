587133cbcd9a3bfe835d6fc6bd62b35a86c2b64f
Valeria Stromtsova <v.stromtsova@yandex.ru>
2020-11-14 17:47:43 +0300

    Backend integration

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import API from "../utils/api";
+
+export interface IUser {
+    id: number;
+    nickname: string;
+    min_donation: string;
+    account: boolean;
+}
 
 function ProfilePage() {
-    return <h2 className="mb-4">Личный кабинет</h2>
+    const [user, setUser] = useState<IUser | undefined>(undefined);
+
+    useEffect(() => {
+        API
+            .get<IUser>("/donate/ivan55off")
+            .then(response => {
+                const {data} = response;
+                setUser(data);
+            })
+            .catch(ex => {console.log(ex)});
+    }, []);
+
+    console.log(user);
+
+    return (<div>
+        <h2 className="mb-4">Личный кабинет</h2>
+        {user ? (user.nickname) : "Кажется, я вас не знаю"}
+    </div>)
 }
 
 export default ProfilePage
