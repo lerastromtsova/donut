95b025d997bd92ae7f157a4b737fd4f677ff46a1
Valeria Stromtsova <v.stromtsova@yandex.ru>
2020-11-14 16:24:07 +0300

    Nav fix

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,12 @@ function App() {
           <Sidebar/>
           <div className="main p-4 p-md-5 pt-5">
               <Switch>
-                  <Route path='/profile' component={ProfilePage} />
+                  <Route path='/donut/profile' component={ProfilePage} />
                   <Route exact path='/donate' component={DonateLinkPage} />
                   <Route exact path='/goals' component={GoalsPage} />
                   <Route exact path='/notifications' component={NotificationsPage} />
                   <Route exact path='/stats' component={StatsPage} />
                   <Route path='/donate_to' component={DonateFormPage} />
-                  <Redirect to='/' />
               </Switch>
           </div>
       </div>
