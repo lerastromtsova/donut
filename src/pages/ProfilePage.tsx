abfc05cfe02fcd1118e428779618ff80d646920e
Petr <revyakinpa@ya.ru>
2020-11-14 21:17:02 +0300

    first try

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -5,7 +5,7 @@ export interface IUser {
     id: number;
     nickname: string;
     min_donation: string;
-    account: boolean;
+    account: string;
 }
 
 function ProfilePage() {
@@ -21,11 +21,46 @@ function ProfilePage() {
             .catch(ex => {console.log(ex)});
     }, []);
 
+    function changeProfile() {
+        console.log('test');
+    }
+
     console.log(user);
 
     return (<div>
                 <h2 className="mb-4">Личный кабинет</h2>
-                {user ? (user.nickname) : "Кажется, я вас не знаю"}
+                <table className="table table-striped">
+                    <tbody>
+                        <tr>
+                            <td>Никнейм</td>
+                            <td>
+                                <input
+                                    name="nickname"
+                                    type="text" 
+                                    value={user?.nickname} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Минимальная сумма доната</td>
+                            <td>
+                                <input 
+                                    name="min_donation"
+                                    value={user ? (user.min_donation) : ""} 
+                                    step="0.01" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Счет</td>
+                            <td>
+                                <input 
+                                    name="account"
+                                    type="text" 
+                                    value={user ? (user.account) : ''} />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <button className="btn btn-primary" onClick={changeProfile}>Сохранить изменения</button>
             </div>)
 }
 
