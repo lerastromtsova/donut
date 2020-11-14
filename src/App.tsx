a088a36a80688e3ec2ed3ae5601392eb24bfebf6
Valeria Stromtsova <v.stromtsova@yandex.ru>
2020-11-14 15:30:19 +0300

    Revert "Revert "Improved navigation and added /streamer and /donate_to endpoints""
    This reverts commit fbf631b081524e4588c800b93abf63919b90a67e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
+
+import StreamerPage from "./pages/StreamerPage";
+import DonateToStreamerPage from "./pages/DonateToStreamerPage";
 
 import "./App.css"
 
-import Sidebar from "./components/Sidebar";
-import ProfilePage from "./pages/ProfilePage";
-import DonateLinkPage from "./pages/DonateLinkPage";
-import GoalsPage from "./pages/GoalsPage";
-import NotificationsPage from "./pages/NotificationsPage";
-import StatsPage from "./pages/StatsPage";
-
 function App() {
   return (
-      <div className="wrapper d-flex align-items-stretch">
-          <Sidebar/>
-          <div className="main p-4 p-md-5 pt-5">
-              <Switch>
-                  <Route path='/profile' component={ProfilePage} />
-                  <Route exact path='/donate' component={DonateLinkPage} />
-                  <Route exact path='/goals' component={GoalsPage} />
-                  <Route exact path='/notifications' component={NotificationsPage} />
-                  <Route exact path='/stats' component={StatsPage} />
-                  <Redirect to='/' />
-              </Switch>
-          </div>
-      </div>
+      <Switch>
+          <Route path='/streamer' component={StreamerPage} />
+          <Route path='/donate_to/:nickname' component={DonateToStreamerPage} />
+      </Switch>
   );
 }
 
