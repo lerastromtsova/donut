380d8eac223f4e32efabdff90f2ff034dc4c1235
Valeria Stromtsova <v.stromtsova@yandex.ru>
2020-11-15 17:15:59 +0300

    Sidebar fix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,24 +9,21 @@ function Sidebar() {
 
     return (
         <>
-        { location.pathname.includes("/lastdonation") ? null :
+        { (location.pathname.includes("/lastdonation") ||
+          location.pathname.includes("/donate_to") ||
+          location.pathname.includes("/success") ) ? null :
         <Nav id="sidebar" className="active">
             <div className="p-4">
                 <h1><a href="/" className="logo">donut</a></h1>
-                    {location.pathname.includes("/donate_to") || location.pathname.includes("/success") ?
-                        null :
-                        (
-                            <ul className="list-unstyled components mb-5"><li className={location.pathname === "/profile" ? "active" : ""}>
-                            <Link to="/profile"><BiHomeAlt className="mr-3"/>Личный кабинет</Link>
+                    <ul className="list-unstyled components mb-5"><li className={location.pathname === "/profile" ? "active" : ""}>
+                        <Link to="/profile"><BiHomeAlt className="mr-3"/>Личный кабинет</Link>
                         </li>
                         <li className={location.pathname === "/notifications" ? "active" : ""}>
                         <Link to="/notifications"><BiBell className="mr-3"/>Уведомления</Link>
                         </li>
                         <li className={location.pathname === "/stats" ? "active" : ""}>
                         <Link to="/stats"><BiBarChartAlt2 className="mr-3"/>Донаты</Link>
-                        </li> </ul>)
-                    }
-
+                        </li> </ul>
                 <div className="footer">
                     <div className="mb-5">
                         <h6 className="text-white-50">Служба поддержки</h6>
