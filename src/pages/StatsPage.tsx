46e3b232de8b28e976c004c6f08f3ca692217276
Valeria Stromtsova <v.stromtsova@yandex.ru>
2020-11-15 13:40:20 +0300

    Stat page improvements

--- src/pages/StatsPage.tsx
+++ src/pages/StatsPage.tsx
@@ -1,12 +1,14 @@
 import React, {ChangeEvent, useEffect, useState} from "react";
 import API from "../utils/api";
+import {Col, Row} from "react-bootstrap";
 
 export interface IDonate {
     id: number,
     name: string,
     streamer: number,
     text: string,
-    amount: string
+    amount: string,
+    create_date: string
 }
 
 function StatsPage() {
@@ -18,8 +20,7 @@ function StatsPage() {
             .get<Array<IDonate>>("/alldonation?id=1")
             .then(response => {
                 const {data} = response;
-                setDonates(data);
-                console.log(data.reverse);
+                setDonates(data.reverse());
             })
             .catch(ex => {console.log(ex)});
     }, []);
@@ -30,6 +31,10 @@ function StatsPage() {
             <div className="card-body">
                 <h5 className="card-title">{donate.name}</h5>
                 <p className="card-text">{donate.amount} рублей</p>
+                <p className="card-text">
+                    {donate.create_date.split('T')[0]}<br/>
+                    {donate.create_date.split('T')[1].slice(0,8)}
+                </p>
                 <p className="card-text donate-text">
                     {donate.text}
                 </p>
@@ -38,11 +43,15 @@ function StatsPage() {
     );
 
     return (
-        <div>
+        <>
             <h2 className="mb-4">Статистика</h2>
-            <h2 className="mb-4">Донаты</h2>
-            { donateItems }
-        </div>
+            <h5 className="mb-4">Донаты</h5>
+            <Row className="w-100">
+                <Col>
+                    { donateItems }
+                </Col>
+            </Row>
+        </>
     )
 }
 
